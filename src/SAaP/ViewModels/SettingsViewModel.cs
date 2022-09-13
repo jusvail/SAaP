47fb089ba9abe44b7409458af1ee7596ee4e25fc
ryoukou <i@hling.org>
2022-09-13 20:50:06 +0800

    2022.9.13@ryoukou version message added

--- src/SAaP/ViewModels/SettingsViewModel.cs
+++ src/SAaP/ViewModels/SettingsViewModel.cs
@@ -4,6 +4,10 @@ using CommunityToolkit.Mvvm.Input;
 using SAaP.Contracts.Services;
 using WinRT.Interop;
 using Microsoft.UI.Xaml;
+using SAaP.Helper;
+using System.Reflection;
+using Windows.ApplicationModel;
+using SAaP.Extensions;
 
 namespace SAaP.ViewModels;
 
@@ -11,6 +15,11 @@ public class SettingsViewModel : ObservableRecipient
 {
     private string _tdxInstallationPath;
     private string _pythonInstallationPath;
+    private string _versionDescription;
+
+    private readonly ILocalSettingsService _localSettingsService;
+
+    private readonly IWindowManageService _windowManageService;
 
     public string PythonInstallationPath
     {
@@ -23,23 +32,43 @@ public class SettingsViewModel : ObservableRecipient
         get => _tdxInstallationPath;
         set => SetProperty(ref _tdxInstallationPath, value);
     }
+    public string VersionDescription
+    {
+        get => _versionDescription;
+        set => SetProperty(ref _versionDescription, value);
+    }
 
     public IAsyncRelayCommand<object> OnPythonInstallationPathPressed { get; }
 
     public IAsyncRelayCommand<object> OnTdxInstallationPathPressed { get; }
-
-    private readonly ILocalSettingsService _localSettingsService;
-
-    private readonly IWindowManageService _windowManageService;
-
 
     public SettingsViewModel(ILocalSettingsService localSettingsService, IWindowManageService windowManageService)
     {
         _localSettingsService = localSettingsService;
         _windowManageService = windowManageService;
 
+        _versionDescription = GetVersionDescription();
+
         OnPythonInstallationPathPressed = new AsyncRelayCommand<object>(OnPythonInstallationPath);
         OnTdxInstallationPathPressed = new AsyncRelayCommand<object>(OnTdxInstallationPath);
+    }
+
+    private static string GetVersionDescription()
+    {
+        Version version;
+
+        if (RuntimeHelper.IsMSIX)
+        {
+            var packageVersion = Package.Current.Id.Version;
+
+            version = new Version(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
+        }
+        else
+        {
+            version = Assembly.GetExecutingAssembly().GetName().Version!;
+        }
+
+        return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
     }
 
     public async Task ReadSettingsWhenInitialize()
@@ -74,8 +103,6 @@ public class SettingsViewModel : ObservableRecipient
     private async Task<string> PickFromFileDirectory(XamlRoot element)
     {
         var window = _windowManageService.GetWindowForElement(element);
-
-        if (window == null) return null;
 
         var folderPicker = new FolderPicker();
 
